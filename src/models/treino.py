45441e7fe2d87940f8fa646ae8203a18ec73a6b3
bernardothives <bernardoalvesthives@gmail.com>
2025-05-26 08:30:38 -0300

    fix model treino

--- src/models/treino.py
+++ src/models/treino.py
@@ -2,60 +2,89 @@ from datetime import date
 from .usuario import Usuario
 
 class Treino:
-    def __init__(self, id_treino: int = None, descricao: str = None, duracao: int = None,
-                 imagem: str = None, usuario: Usuario = None, data_treino: date = None,
-                 curtidas: int = 0):
-        # Atributos conforme o diagrama [cite: 1]
-        self.id: int = id_treino
-        self.descricao: str = descricao
-        self.duracao: int = duracao # Em minutos, por exemplo
-        self.imagem: str = imagem
-        self.usuario: Usuario = usuario
-        self.data: date = data_treino
-        self.curtidas: int = curtidas
+    def __init__(
+        self,
+        id_treino: int = None,
+        descricao: str = None,
+        duracao: int = None,
+        imagem: str = None,
+        usuario: Usuario = None,
+        data_treino: date = None,
+        curtidas: int = 0
+    ):
+        # Atributos privados internos
+        self._id: int = id_treino
+        self._descricao: str = descricao
+        self._duracao: int = duracao  # Em minutos
+        self._imagem: str = imagem
+        self._usuario: Usuario = usuario
+        self._data: date = data_treino
+        self._curtidas: int = curtidas
 
-    # --- Getters ---
-    def getCurtidas(self) -> int: # [cite: 1]
-        return self.curtidas
+    @property
+    def id(self) -> int:
+        """ID do treino (somente leitura)"""
+        return self._id
 
-    def getData(self) -> date: # [cite: 1]
-        return self.data
+    @property
+    def descricao(self) -> str:
+        """Descrição do treino"""
+        return self._descricao
 
-    def getDescricao(self) -> str: # [cite: 1]
-        return self.descricao
+    @descricao.setter
+    def descricao(self, valor: str) -> None:
+        self._descricao = valor
 
-    def getDuracao(self) -> int: # [cite: 1]
-        return self.duracao
+    @property
+    def duracao(self) -> int:
+        """Duração do treino em minutos"""
+        return self._duracao
 
-    def getImagem(self) -> str: # Nome corrigido de getimagem para getImagem [cite: 1]
-        return self.imagem
+    @duracao.setter
+    def duracao(self, valor: int) -> None:
+        self._duracao = valor
 
-    def getUsuario(self) -> Usuario: # [cite: 1]
-        return self.usuario
+    @property
+    def imagem(self) -> str:
+        """Caminho ou nome da imagem associada ao treino"""
+        return self._imagem
 
-    # --- Setters ---
-    def setCurtidas(self, curtidas: int) -> None: # [cite: 1]
-        self.curtidas = curtidas
+    @imagem.setter
+    def imagem(self, valor: str) -> None:
+        self._imagem = valor
 
-    def setData(self, data_treino: date) -> None: # [cite: 1]
-        self.data = data_treino
+    @property
+    def usuario(self) -> Usuario:
+        """Usuário que realizou o treino"""
+        return self._usuario
 
-    def setDescricao(self, descricao: str) -> None: # [cite: 1]
-        self.descricao = descricao
+    @usuario.setter
+    def usuario(self, valor: Usuario) -> None:
+        self._usuario = valor
 
-    def setDuracao(self, duracao: int) -> None: # [cite: 1]
-        self.duracao = duracao
+    @property
+    def data(self) -> date:
+        """Data em que o treino foi realizado"""
+        return self._data
 
-    def setImagem(self, imagem: str) -> None: # Nome corrigido de setimagem para setImagem [cite: 2]
-        self.imagem = imagem
+    @data.setter
+    def data(self, valor: date) -> None:
+        self._data = valor
 
-    def setUsuario(self, usuario: Usuario) -> None: # [cite: 2]
-        self.usuario = usuario
+    @property
+    def curtidas(self) -> int:
+        """Número de curtidas recebidas pelo treino"""
+        return self._curtidas
+
+    @curtidas.setter
+    def curtidas(self, valor: int) -> None:
+        self._curtidas = valor
 
     def __str__(self) -> str:
-        """ Retorna uma representação em string do objeto Treino. """
+        """Representação em string do objeto Treino."""
+        nome_usuario = self.usuario.getNome() if self.usuario else 'N/A'
         return (
-            f"Treino(ID: {self.id}, Descrição: '{self.descricao}', Duração: {self.duracao} mins, "
-            f"Imagem: '{self.imagem}', Usuário: {self.usuario.getNome() if self.usuario else 'N/A'}, "
-            f"Data: {self.data}, Curtidas: {self.curtidas})"
+            f"Treino(ID: {self.id}, Descrição: '{self.descricao}', "
+            f"Duração: {self.duracao} mins, Imagem: '{self.imagem}', "
+            f"Usuário: {nome_usuario}, Data: {self.data}, Curtidas: {self.curtidas})"
         )
